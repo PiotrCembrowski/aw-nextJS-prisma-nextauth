cc7f855540d971bbd6100c050881fd202a049258
Piotr Cembrowski <losttape@gmail.com>
2024-06-12 18:18:12 +0200

    navbar added and fixed padding of content in layouit

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,8 +1,19 @@
-import React from "react";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav className="w-full fixed top-0 left-0 py-6 border-b bg-white z-10"></nav>
+    <nav className="w-full fixed top-0 left-0 py-6 border-b bg-white z-10">
+      <div className="max-w-[1500px] mx-auto px-6">
+        <div className="flex justify-between items-center">
+          <Link href="/">Around the World</Link>
+
+          <div className="flex space-x-6 ">Search Filter</div>
+          <div className="flex items-center space-x-6">
+            Add property - User nav
+          </div>
+        </div>
+      </div>
+    </nav>
   );
 };
 
