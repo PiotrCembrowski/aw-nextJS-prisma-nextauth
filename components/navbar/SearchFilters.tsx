6710893ce29349b4033ae635e378fb5fd0250372
Piotr Cembrowski <losttape@gmail.com>
2024-06-13 20:45:10 +0200

    navbar finished

--- components/navbar/SearchFilters.tsx
+++ components/navbar/SearchFilters.tsx
@@ -4,28 +4,28 @@ import icon from "@/app/images/search.svg";
 const SearchFilters = () => {
   return (
     <div className="h-[64px] flex flex-row items-center justify-between border rounded-full">
-      <div>
+      <div className="hidden lg:block">
         <div className="flex flex-row items-center justify-between">
-          <div className="w-[250px] h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100">
+          <div className="cursor-pointer w-[250px] h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100">
             <p className="text-xs font-semibold">Where</p>
             <p className="text-sm">Wanted location</p>
           </div>
-          <div className="h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100">
+          <div className="cursor-pointer h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100">
             <p className="text-xs font-semibold">Check in</p>
             <p className="text-sm">Add dates</p>
           </div>
-          <div className="h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100">
+          <div className="cursor-pointer h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100">
             <p className="text-xs font-semibold">Check out</p>
             <p className="text-sm">Add dates</p>
           </div>
-          <div className="h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100">
+          <div className="cursor-pointer h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100">
             <p className="text-xs font-semibold">Who</p>
             <p className="text-sm">Add quests</p>
           </div>
         </div>
       </div>
       <div className="p-2">
-        <div className="p-4 bg-aw rounded-full text-white">
+        <div className="cursor-pointer p-4 bg-aw hover:bg-awDark rounded-full text-white">
           <Image src={icon} alt="search" width="15" height="15" />
         </div>
       </div>
