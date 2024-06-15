4439c068e479d284e73c5a896f721b8e1aeb3b3d
Piotr Cembrowski <losttape@gmail.com>
2024-06-15 19:28:46 +0200

    styling added to category div

--- app/components/Categories.tsx
+++ app/components/Categories.tsx
@@ -1,5 +1,11 @@
 const Categories = () => {
-  return <div>Category</div>;
+  return (
+    <div className="pt-3 cursor-pointer pb-6 flex items-center space-x-12">
+      <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100">
+        adfs
+      </div>
+    </div>
+  );
 };
 
 export default Categories;
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Categories from "./components/Categories";
 
 export default function Home() {
   return (
-    <div>
+    <div className="max-w-[1500px] mx-auto px-6">
       <Categories />
     </div>
   );
