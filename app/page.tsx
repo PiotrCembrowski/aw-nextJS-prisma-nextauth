f95ef9e25faff5d54cc1ed7d33370cd7617c7712
Piotr Cembrowski <losttape@gmail.com>
2024-06-17 13:54:14 +0200

    folders with countries and cities created and countrieslist component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Categories from "./components/Categories";
+import CountriesList from "./components/countries/CountriesList";
 
 export default function Home() {
   return (
     <div className="max-w-[1500px] mx-auto px-6">
       <Categories />
+      <CountriesList />
     </div>
   );
 }
