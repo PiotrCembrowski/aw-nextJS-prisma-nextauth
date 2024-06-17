fa28b2a9ed4c772c574b0774468484b5940dac2f
Piotr Cembrowski <losttape@gmail.com>
2024-06-17 14:33:36 +0200

    image added

--- app/components/countries/CountriesLisItem.tsx
+++ app/components/countries/CountriesLisItem.tsx
@@ -1,5 +1,20 @@
+import Image from "next/image";
+import AsiaImg from "@/public/images/asia.jpg";
+
 const CountriesLisItem = () => {
-  return <div>Country</div>;
+  return (
+    <div className="cursor-pointer">
+      <div className="relative overflow-hidden aspect-square rounded-xl">
+        <Image
+          fill
+          src={AsiaImg}
+          sizes="(max-width: 768px) 768px, (max-width: 1200px):768px, 768px"
+          className="hover:scale-110 object-cover transition h-full w-full"
+          alt="Asia"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default CountriesLisItem;
--- app/components/navbar/SearchFilters.tsx
+++ app/components/navbar/SearchFilters.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import icon from "@/app/images/search.svg";
+import icon from "@/public/images/search.svg";
 
 const SearchFilters = () => {
   return (
