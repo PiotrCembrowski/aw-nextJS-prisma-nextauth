8a40ce644406c82c390e07158dfa15fbcd56f3dc
Piotr Cembrowski <losttape@gmail.com>
2024-06-17 13:47:41 +0200

    Categories added

--- app/components/Categories.tsx
+++ app/components/Categories.tsx
@@ -1,8 +1,25 @@
+import Link from "next/link";
+
 const Categories = () => {
   return (
     <div className="pt-3 cursor-pointer pb-6 flex items-center space-x-12">
       <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100">
-        adfs
+        <Link href="/">Asia</Link>
+      </div>
+      <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100">
+        <Link href="/">Europe</Link>
+      </div>
+      <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100">
+        <Link href="/">North America</Link>
+      </div>
+      <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100">
+        <Link href="/">South America</Link>
+      </div>
+      <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100">
+        <Link href="/">Africa</Link>
+      </div>
+      <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100">
+        <Link href="/">Austalia & Oceania</Link>
       </div>
     </div>
   );
