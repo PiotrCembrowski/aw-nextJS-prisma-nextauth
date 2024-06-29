416f05ce660caa919fb38b63972bf831b7cacb0e
Piotr Cembrowski <losttape@gmail.com>
2024-06-29 19:56:39 +0200

    button added

---
+++ app/components/Button.tsx
@@ -0,0 +1,13 @@
+interface Props {
+  title: string;
+}
+
+const ContactButton: React.FC<Props> = ({ title }) => {
+  return (
+    <div className="mt-6 py-4 px-6 bg-aw text-white rounded-xl cursor-pointer hover:bg-awDark">
+      {title}
+    </div>
+  );
+};
+
+export default ContactButton;
---
+++ app/users/[id]/page.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import Button from "@/app/components/Button";
+
+const UserDetailPage: React.FC = () => {
+  return (
+    <main className="max-w-[1500px] mx-auto px-6 pb-6">
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+        <aside className="cols-span-1 mb-4 ">
+          <div className="flex flex-col items-center p-6 rounded-xl border border-gray-300 shadow-xl">
+            <Image
+              src="/images/user.png"
+              width={200}
+              height={500}
+              alt="user"
+              className="rounded-full"
+            />
+            <h1 className="mt-6 text-2xl">User Name</h1>
+
+            <Button title="preview" />
+          </div>
+        </aside>
+
+        <div className="col-span-3 pl-0 md:pl-6">Travel visits</div>
+      </div>
+    </main>
+  );
+};
+
+export default UserDetailPage;
