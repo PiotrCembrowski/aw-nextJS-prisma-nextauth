c27f2f76ebe633ded74bf5e26a0da50c6fdd6883
Piotr Cembrowski <losttape@gmail.com>
2024-07-04 20:46:44 +0200

    creating posts route and page

---
+++ app/posts/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+const PostsPage = () => {
+  return (
+    <main className="max-w-[1500px] mx-auto px-6 pb-6">
+      <div className="pt-6 pb-2">
+        <h1 className="text-2xl">My posts</h1>
+      </div>
+    </main>
+  );
+};
+
+export default PostsPage;
