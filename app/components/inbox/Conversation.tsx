bedfa29574273faeeaa04d9a31efa04fa26e153d
Piotr Cembrowski <losttape@gmail.com>
2024-07-05 20:22:04 +0200

    conversation component added

---
+++ app/components/inbox/Conversation.tsx
@@ -0,0 +1,11 @@
+const Conversation = () => {
+  return (
+    <div className="px-6 py-4 border border-gray-300 rounded-xl">
+      <p className="mb-6 text-xl">John Doe</p>
+
+      <p className="text-awDark cursor-pointer">Go to Conversation</p>
+    </div>
+  );
+};
+
+export default Conversation;
