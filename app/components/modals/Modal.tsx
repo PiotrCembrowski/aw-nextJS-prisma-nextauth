ac4acbacce5b6d7d4de06b29314a5c6118a9024a
Piotr Cembrowski <losttape@gmail.com>
2024-07-10 14:45:07 +0200

    basic modal

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -1,7 +1,15 @@
 "use client";
 
-const Modal: React.FC<> = () => {
-  return <div>Modal</div>;
+const Modal: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center fixed inset-0 z-50 bg-black/60">
+      <div className="relative w-[90%] md:w-[80%] lg:w-[700px] my-6 h-auto">
+        <div
+          className={`translate duration-600 h-full translate-y-0 opacity-100`}
+        ></div>
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
