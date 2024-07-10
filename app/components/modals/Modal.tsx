60d07c27dbc0c4ace7c2ca1f82736a4e28b98493
Piotr Cembrowski <losttape@gmail.com>
2024-07-10 14:55:33 +0200

    header and close btn added to modal

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -6,7 +6,15 @@ const Modal: React.FC = () => {
       <div className="relative w-[90%] md:w-[80%] lg:w-[700px] my-6 h-auto">
         <div
           className={`translate duration-600 h-full translate-y-0 opacity-100`}
-        ></div>
+        >
+          <div className="w-full h-auto rounded-xl relative flex flex-col bg-white">
+            <header className="flex items-center p-6 rounded-t justify-center relative border-b">
+              <div className="p-3 absolute left-3 hover:bg-gray-300 rounded-full cursor-pointer">
+                Close
+              </div>
+            </header>
+          </div>
+        </div>
       </div>
     </div>
   );
