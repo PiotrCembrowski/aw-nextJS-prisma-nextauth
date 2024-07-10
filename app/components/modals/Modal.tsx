413928ccebb54cf418551642a6cef20e2d3cbd91
Piotr Cembrowski <losttape@gmail.com>
2024-07-10 15:30:01 +0200

    header fnished

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -8,11 +8,30 @@ const Modal: React.FC = () => {
           className={`translate duration-600 h-full translate-y-0 opacity-100`}
         >
           <div className="w-full h-auto rounded-xl relative flex flex-col bg-white">
-            <header className="flex items-center p-6 rounded-t justify-center relative border-b">
-              <div className="p-3 absolute left-3 hover:bg-gray-300 rounded-full cursor-pointer">
-                Close
+            <header className="h-[60px] flex items-center p-6 rounded-t justify-center relative border-b">
+              <div className="p-3 absolute right-3 hover:bg-gray-300 rounded-full cursor-pointer">
+                <svg
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18 18 6M6 6l12 12"
+                  />
+                </svg>
               </div>
+              <h2 className="text-ld font-bold">The Title</h2>
             </header>
+
+            <section className="p-6">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo
+              incidunt consectetur quod odio animi repellat velit reprehenderit
+              minima necessitatibus enim?
+            </section>
           </div>
         </div>
       </div>
