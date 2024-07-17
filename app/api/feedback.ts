1cc8a10709c25744952f0c3eda775aec5bcdcda0
Piotr Cembrowski <losttape@gmail.com>
2024-07-17 12:07:14 +0200

    routes added to api properly working

--- app/api/feedback.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-// To handle a GET request to /api
-export async function GET(request) {
-  // Do whatever you want
-  return NextResponse.json({ message: "Hello World" }, { status: 200 });
-}
-
-//conflict of routes with cities
--- app/api/route.ts
+++ app/api/route.ts
@@ -3,5 +3,5 @@ import { NextResponse } from "next/server";
 // To handle a GET request to /api
 export async function GET(request) {
   // Do whatever you want
-  return NextResponse.json({ message: "Hello World" }, { status: 200 });
+  return NextResponse.json({ message: "It works!" }, { status: 200 });
 }
