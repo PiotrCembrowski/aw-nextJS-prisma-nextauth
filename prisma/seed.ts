2979235f3f60655e46107787093d71caecfcde5c
Piotr Cembrowski <losttape@gmail.com>
2024-07-27 14:50:16 +0200

    prisma function for seeding added

---
+++ prisma/seed.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from "@prisma/client/extension";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.upsert({
+    where: { email: "losttape@gmail.com" },
+    update: {},
+    create: {
+      email: "losttape@gmail.com",
+      name: "Test User",
+      password: `1234`,
+    },
+  });
+  console.log({ user });
+}
+
+main()
+  .then(() => prisma.$disconnect())
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
