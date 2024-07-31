b81c39e10975e2d430cbdfb3954e06adfe994167
Piotr Cembrowski <losttape@gmail.com>
2024-07-31 14:01:17 +0200

    handler for auth res added

--- app/api/auth/signup.ts
+++ app/api/auth/signup.ts
@@ -1,5 +1,22 @@
 import prisma from "@/lib/prisma";
 
-function handler() {}
+function handler(req: Request, res: Response) {
+  const data = req.body;
+
+  const { email, password } = data;
+
+  if (
+    !email ||
+    !email.includes("@") ||
+    !password ||
+    password.trim().length < 7
+  ) {
+    res.status(422).json({
+      message:
+        "Invalid input - password should also be at least 7 characters long.",
+    });
+    return;
+  }
+}
 
 export default handler;
