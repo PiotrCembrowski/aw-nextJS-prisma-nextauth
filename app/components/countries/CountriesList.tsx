88e11155aecaa19c6546624a3a07e3b47fea210f
Piotr Cembrowski <losttape@gmail.com>
2024-08-03 14:32:42 +0200

    get contries and useeffect added

--- app/components/countries/CountriesList.tsx
+++ app/components/countries/CountriesList.tsx
@@ -1,6 +1,17 @@
+"use client";
+
+import { useEffect } from "react";
 import CountriesLisItem from "./CountriesLisItem";
 
 const CountriesList = () => {
+  const getCountries = async () => {
+    const url = "http://localhost:3000/api/countries";
+  };
+
+  useEffect(() => {
+    getCountries();
+  }, []);
+
   return (
     <>
       <CountriesLisItem />
