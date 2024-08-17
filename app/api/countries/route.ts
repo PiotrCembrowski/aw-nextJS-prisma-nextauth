5a0bbdee9ec13ae902fd86b5ae57b06149053066
Piotr Cembrowski <losttape@gmail.com>
2024-08-17 13:32:56 +0200

    adding cashing to route

--- app/api/countries/route.ts
+++ app/api/countries/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
 
-export async function GET(request) {
+export const dynamic = "force-static";
+
+export async function GET(request: Request) {
   return NextResponse.json({ message: "countries list" }, { status: 200 });
 }
