0a244c24900245123ce5ea5e40bac408928b750b
Piotr Cembrowski <losttape@gmail.com>
2024-08-19 14:10:36 +0200

    post func configured

--- app/api/countries/route.ts
+++ app/api/countries/route.ts
@@ -2,13 +2,12 @@ import { NextResponse } from "next/server";
 
 export const dynamic = "force-static";
 
-export async function GET(request: Request) {
+export async function GET(req: Request, res: Response) {
   return NextResponse.json({ message: "countries list" }, { status: 200 });
 }
 
-export async function POST(request: Request) {
-  return NextResponse.json(
-    { message: "You have succsefullu post country info" },
-    { status: 200 }
-  );
+export async function POST(request: Request, reesponse: Response) {
+  const formData = await request.formData();
+  const name = formData.get("name");
+  return Response.json({ name });
 }
