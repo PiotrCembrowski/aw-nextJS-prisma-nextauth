13c9d37b9ae6abecadd27ec35793733e1df46661
Piotr Cembrowski <losttape@gmail.com>
2024-08-19 13:42:51 +0200

    adding post func in api route countries

--- app/api/countries/route.ts
+++ app/api/countries/route.ts
@@ -5,3 +5,10 @@ export const dynamic = "force-static";
 export async function GET(request: Request) {
   return NextResponse.json({ message: "countries list" }, { status: 200 });
 }
+
+export async function POST(request: Request) {
+  return NextResponse.json(
+    { message: "You have succsefullu post country info" },
+    { status: 200 }
+  );
+}
