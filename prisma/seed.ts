218a76663ea664b37a1a9f94277fa530bb44c68f
Piotr Cembrowski <losttape@gmail.com>
2024-08-20 15:58:43 +0200

    another bug fixed in seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,33 +1,35 @@
-import { PrismaClient, Prisma } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-const userData: Prisma.UserCreateInput[] = [
-  {
-    email: "losttape@gmail.com",
-    password: "1234",
-    name: "Piotr",
-    posts: {
-      create: [
-        {
-          title: "New post",
-          content: "New content",
-          published: false,
-        },
-      ],
-    },
-  },
-];
-
 async function main() {
+  // const user = await prisma.user.create({
+  //   data: {
+  //     email: "losttape@gmail.com",
+  //     password: "1234",
+  //     name: "Piotr",
+  //     posts: {
+  //       create: {
+  //         title: "Check out Prisma with Next.js",
+  //         content: "https://www.prisma.io/nextjs",
+  //         published: false,
+  //       },
+  //     },
+  //   },
+  // });
+
   console.log("Start seeding...");
-  for (const u of userData) {
-    const user = await prisma.user.create({
-      data: u,
-    });
-    console.log(`Created user with id: ${user.id}`);
-  }
+  // console.log(`Created user with id: ${user.id}`);
+
   console.log(`Seeding finished.`);
+
+  const userRead = await prisma.user.findUnique({
+    where: {
+      email: "losttape@gmail.com",
+    },
+  });
+
+  console.log(userRead);
 }
 
 main()
