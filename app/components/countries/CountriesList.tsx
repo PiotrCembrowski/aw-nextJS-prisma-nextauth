a087f54aa594b27f6642d3ba4caef1237f0e71d0
Piotr Cembrowski <losttape@gmail.com>
2024-08-22 13:50:07 +0200

    adding usestate and useeffect to store data

--- app/components/countries/CountriesList.tsx
+++ app/components/countries/CountriesList.tsx
@@ -3,14 +3,21 @@
 import { useQuery } from "@tanstack/react-query";
 import CountriesLisItem from "./CountriesLisItem";
 import { getCountries } from "@/lib/fetchCountries";
+import { useEffect, useState } from "react";
 
 const CountriesList = () => {
+  const [countryList, setCountryList] = useState<string | undefined>();
+
   const { data } = useQuery({
     queryKey: ["Countries"],
     queryFn: getCountries,
   });
 
-  console.log(data);
+  useEffect(() => {
+    setCountryList(data);
+  }, [data, countryList]);
+
+  console.log(countryList);
 
   return (
     <>
