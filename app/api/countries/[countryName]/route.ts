3bf69ef94d2e6e3a149005544eb09f6d6b6b444c
Piotr Cembrowski <losttape@gmail.com>
2024-08-22 16:12:37 +0200

    adding dynamic params and usepathname

--- app/api/countries/[countryName]/route.ts
+++ app/api/countries/[countryName]/route.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+import { usePathname } from "next/navigation";
 import { NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
@@ -8,8 +9,11 @@ export const dynamic = "force-static";
 export const dynamicParams = true;
 
 export async function GET(req: Request, res: Response) {
+  const pathName = usePathname();
   const name = req.url;
+
   console.log(name);
+  return NextResponse.json({ message: `country of ${name}` }, { status: 200 });
 }
 
 export async function DELETE(req: Request, res: Response) {
