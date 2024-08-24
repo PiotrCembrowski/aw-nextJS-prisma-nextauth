4f76da351732f7dca0668dfdca91f0c472ac801f
Piotr Cembrowski <losttape@gmail.com>
2024-08-24 14:24:18 +0200

    matchers added

--- middleware.ts
+++ middleware.ts
@@ -8,5 +8,37 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: "/api/countries/:countryName",
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    {
+      source:
+        "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+      missing: [
+        { type: "header", key: "next-router-prefetch" },
+        { type: "header", key: "purpose", value: "prefetch" },
+      ],
+    },
+
+    {
+      source:
+        "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+      has: [
+        { type: "header", key: "next-router-prefetch" },
+        { type: "header", key: "purpose", value: "prefetch" },
+      ],
+    },
+
+    {
+      source:
+        "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+      has: [{ type: "header", key: "x-present" }],
+      missing: [{ type: "header", key: "x-missing", value: "prefetch" }],
+    },
+  ],
 };
