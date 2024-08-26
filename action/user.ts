ef5224829bdb419b9edf04de7bfb13fec16d820a
Piotr Cembrowski <losttape@gmail.com>
2024-08-26 16:52:31 +0200

    adding validation for inputs

--- action/user.ts
+++ action/user.ts
@@ -1,10 +1,14 @@
 "use server";
 
 const register = async (formData: FormData) => {
-  const name = formData.get("name") as String;
+  const username = formData.get("username") as String;
   const email = formData.get("useremail") as String;
   const password = formData.get("userpassword") as String;
   const passwordRepeated = formData.get("userpasswordrepeated") as String;
+
+  if (!username || !email || !password || !passwordRepeated) {
+    throw new Error("Please fill all fields.");
+  }
 };
 
 export { register };
