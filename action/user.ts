ac7d53816a5d0c8131ca61a2ac5ccf72db502843
Piotr Cembrowski <losttape@gmail.com>
2024-08-26 16:38:01 +0200

    added vars for formdata

--- action/user.ts
+++ action/user.ts
@@ -1,5 +1,7 @@
 "use server";
 
 const register = async (formData: FormData) => {
-  const email = formData.get("useremail");
+  const email = formData.get("useremail") as String;
+  const password = formData.get("userpassword") as String;
+  const passwordRepeated = formData.get("userpasswordrepeated") as String;
 };
