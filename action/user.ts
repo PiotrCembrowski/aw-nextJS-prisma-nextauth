4b788544f37c56adff2abff24d5190cc409cf57d
Piotr Cembrowski <losttape@gmail.com>
2024-08-26 16:45:31 +0200

    adding name formdata to register func

--- action/user.ts
+++ action/user.ts
@@ -1,6 +1,7 @@
 "use server";
 
 const register = async (formData: FormData) => {
+  const name = formData.get("name") as String;
   const email = formData.get("useremail") as String;
   const password = formData.get("userpassword") as String;
   const passwordRepeated = formData.get("userpasswordrepeated") as String;
