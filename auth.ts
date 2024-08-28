8f885e62481ba9ace2298ff1ce70cfb6666ae249
Piotr Cembrowski <losttape@gmail.com>
2024-08-28 14:31:23 +0200

    fixing typo

--- auth.ts
+++ auth.ts
@@ -2,31 +2,33 @@ import { PrismaClient } from "@prisma/client";
 import NextAuth, { CredentialsSignin } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 
-const prisma = new PrismaClient;
+const prisma = new PrismaClient();
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Credentials({
       name: "Credentials",
       credentials: {
-        emaail: { label: "Email", type: "email" },
+        email: { label: "Email", type: "email" },
         password: { label: "Password", type: "password" },
       },
+      authorize: async (credentials) => {
+        const email = credentials.email as string | undefined;
+        const password = credentials.password as string | undefined;
+
+        if (!email || !password) {
+          throw new CredentialsSignin(
+            "Please provide both email and password."
+          );
+        }
+
+        const existingUser = await prisma.user.findUnique({
+          where: {
+            email: email,
+            password: password,
+          },
+        });
+      },
     }),
-    authorize: async(credentials) => {
-      const email = credentials.emaail as string | undefined;
-      const password = credentials.password as string | undefined;
-
-      if(!email || !password) {
-        throw new CredentialsSignin('Please provide both email and password.')
-      }
-
-      const existingUser = await prisma.user.findUnique({
-        where: {
-          email: email,
-          password: password,
-        },
-      });
-    }
   ],
 });
