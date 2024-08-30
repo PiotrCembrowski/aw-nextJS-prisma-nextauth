09cdf7e407f908f7ec040d785bda340b2f49357d
Piotr Cembrowski <losttape@gmail.com>
2024-08-30 16:24:44 +0200

    adding modal handler

--- app/components/navbar/AddReportButton.tsx
+++ app/components/navbar/AddReportButton.tsx
@@ -1,4 +1,11 @@
+import useAddReportModal from "@/app/hooks/UseReportModal";
+import AddReportModal from "../modals/AddReportModal";
+
 const AddPropertyButton = () => {
+  const addReportModal = useAddReportModal();
+  const ArountTheWorldHome = () => {
+    addReportModal.open();
+  };
   return (
     <div className="p-2 cursor-pointer text-sm font-semibold rounded-full hover:bg-gray-200">
       Around The World Home
--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import SearchFilters from "./SearchFilters";
 import UserNav from "./UserNav";
-import AddPropertyButton from "./AddPropertyButton";
+import AddPropertyButton from "./AddReportButton";
 
 const Navbar = () => {
   return (
