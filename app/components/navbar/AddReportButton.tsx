37a81be9c95217c1db088573a2d7b09b7e40c285
Piotr Cembrowski <losttape@gmail.com>
2024-08-31 15:09:54 +0200

    adding interface fr report button and loginmodal logic

--- app/components/navbar/AddReportButton.tsx
+++ app/components/navbar/AddReportButton.tsx
@@ -1,12 +1,24 @@
 "use client";
 
+import useLoginModal from "@/app/hooks/UseLoginModa";
 import useAddReportModal from "@/app/hooks/UseAddReportModal";
 
-const AddReportButton = () => {
+interface AddReportButtonProps {
+  userId?: string | null;
+}
+
+const AddReportButton: React.FC<AddReportButtonProps> = ({ userId }) => {
+  const loginModal = useLoginModal();
   const addReportModal = useAddReportModal();
+
   const arountTheWorldHome = () => {
-    addReportModal.open();
+    if (userId) {
+      addReportModal.open();
+    } else {
+      loginModal.open();
+    }
   };
+
   return (
     <div
       onClick={arountTheWorldHome}
