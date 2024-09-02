062899f204db6aab90b2a261c9d19e444de3e259
Piotr Cembrowski <losttape@gmail.com>
2024-09-02 13:57:21 +0200

    logic for fetch auto finished

--- action/fetchAuth.ts
+++ action/fetchAuth.ts
@@ -4,5 +4,9 @@ import { auth } from "@/auth";
 
 export const getSession = async () => {
   const session = await auth();
+  const user = session?.user;
+  if (user) {
+    console.log("user loaded");
+  }
   console.log("--------", session);
 };
