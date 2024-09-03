e8ea33e807b52e5dda9b3d541830cd032d4965a3
Piotr Cembrowski <losttape@gmail.com>
2024-09-03 18:18:42 +0200

    adding currentStep state

--- app/components/modals/AddReportModal.tsx
+++ app/components/modals/AddReportModal.tsx
@@ -1,12 +1,20 @@
 "use client";
 
+import { useState } from "react";
+import CustomButton from "../forms/CustomButton";
 import Modal from "./Modal";
 import useAddReportModal from "@/app/hooks/UseAddReportModal";
 
 const AddReportModal = () => {
+  const [currentSteo, setCurrentStep] = useState();
   const addReportModal = useAddReportModal();
 
-  const content = <p>Yo</p>;
+  const content = (
+    <>
+      <h2 className="mb-6 text-2xl">Choose Category</h2>
+      <CustomButton label="Next" onClick={() => setCurrentStep(2)} />
+    </>
+  );
 
   return (
     <>
