a60e87d5b7fa6e2755fa0246a81f3e2b34da6085
Piotr Cembrowski <losttape@gmail.com>
2024-09-05 14:14:53 +0200

    adding div to reports

--- app/components/addreport/Reports.tsx
+++ app/components/addreport/Reports.tsx
@@ -4,7 +4,13 @@ interface ReportsProps {
 }
 
 const Reports: React.FC<ReportsProps> = ({ dataReport, setReport }) => {
-  return <></>;
+  return (
+    <>
+      <div className="pt-3 cursor-pointer pb-6 flex items-center space-x-12">
+        asd
+      </div>
+    </>
+  );
 };
 
 export default Reports;
