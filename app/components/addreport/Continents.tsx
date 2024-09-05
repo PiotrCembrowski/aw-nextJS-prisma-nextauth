40554d02f37f2ef934302322c2ec4d60f30aaa47
Piotr Cembrowski <losttape@gmail.com>
2024-09-05 14:46:27 +0200

    continents added

--- app/components/addreport/Continents.tsx
+++ app/components/addreport/Continents.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 interface ContinentsProps {
   dataContinents: string;
   setContinents: (country: string) => void;
@@ -10,7 +12,24 @@ const Continents: React.FC<ContinentsProps> = ({
   return (
     <>
       <div className="pt-3 cursor-pointer pb-6 flex items-center space-x-12">
-        asd
+        <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100">
+          <Link href="/">Asia</Link>
+        </div>
+        <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100">
+          <Link href="/">Europe</Link>
+        </div>
+        <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100">
+          <Link href="/">North America</Link>
+        </div>
+        <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100">
+          <Link href="/">South America</Link>
+        </div>
+        <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100">
+          <Link href="/">Africa</Link>
+        </div>
+        <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100">
+          <Link href="/">Austalia & Oceania</Link>
+        </div>
       </div>
     </>
   );
