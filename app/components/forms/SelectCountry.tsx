b1f76cc31eaa2e9d262779b52f49640b135608d2
Piotr Cembrowski <losttape@gmail.com>
2024-09-05 17:08:43 +0200

    exporting type and adding interface

--- app/components/forms/SelectCountry.tsx
+++ app/components/forms/SelectCountry.tsx
@@ -1,6 +1,17 @@
 "use client";
 
 import Select from "react-select";
+import useCountries from "@/app/hooks/useCountries";
+
+export type SelectCountryValue = {
+  label: string;
+  value: string;
+};
+
+interface SelectCountryProps {
+  value?: SelectCountryValue;
+  onChange: (value: SelectCountryValue) => void;
+}
 
 const SelectCountry = () => {
   return <div>SelectCountry</div>;
