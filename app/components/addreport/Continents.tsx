3b326edf397ce8bbb17801b28a85d7b7cb069857
Piotr Cembrowski <losttape@gmail.com>
2024-09-05 14:55:37 +0200

    adding onClick setContinets

--- app/components/addreport/Continents.tsx
+++ app/components/addreport/Continents.tsx
@@ -12,22 +12,40 @@ const Continents: React.FC<ContinentsProps> = ({
   return (
     <>
       <div className="pt-3 cursor-pointer pb-6 flex items-center space-x-10">
-        <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100">
+        <div
+          onClick={() => setContinents("Asia")}
+          className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100"
+        >
           <span className="text-sm">Asia</span>
         </div>
-        <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100">
+        <div
+          onClick={() => setContinents("Europe")}
+          className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100"
+        >
           <span className="text-sm">Europe</span>
         </div>
-        <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100">
+        <div
+          onClick={() => setContinents("North America")}
+          className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100"
+        >
           <span className="text-sm">North America</span>
         </div>
-        <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100">
+        <div
+          onClick={() => setContinents("South America")}
+          className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100"
+        >
           <span className="text-sm">South America</span>
         </div>
-        <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100">
+        <div
+          onClick={() => setContinents("Africa")}
+          className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100"
+        >
           <span className="text-sm">Africa</span>
         </div>
-        <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100">
+        <div
+          onClick={() => setContinents("Austalia & Oceania")}
+          className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100"
+        >
           <span className="text-sm">Austalia & Oceania</span>
         </div>
       </div>
