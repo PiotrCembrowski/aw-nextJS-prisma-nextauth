2d45f15ed711cd6abe167ffef391ba5220f1465d
Piotr Cembrowski <losttape@gmail.com>
2024-09-05 16:51:19 +0200

    useCOuntries func added

--- app/hooks/useCountries.ts
+++ app/hooks/useCountries.ts
@@ -4,3 +4,11 @@ const formattedCountries = countries.map((country) => ({
   value: country.cca2,
   label: country.name.common,
 }));
+
+const useCountries = () => {
+  const getAll = () => formattedCountries;
+
+  const getByValue = (value: string) => {
+    return formattedCountries.find((item) => item.value === value);
+  };
+};
