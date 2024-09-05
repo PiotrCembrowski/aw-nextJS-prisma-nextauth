c02cb84769cf49fade46ec5d96fb7057cc98c48e
Piotr Cembrowski <losttape@gmail.com>
2024-09-05 17:15:41 +0200

    creating datacountry useState and adding it do selectcountry element

--- app/components/modals/AddReportModal.tsx
+++ app/components/modals/AddReportModal.tsx
@@ -5,6 +5,7 @@ import CustomButton from "../forms/CustomButton";
 import Modal from "./Modal";
 import useAddReportModal from "@/app/hooks/UseAddReportModal";
 import Continents from "../addreport/Continents";
+import SelectCountry from "../forms/SelectCountry";
 
 const AddReportModal = () => {
   //
@@ -13,6 +14,7 @@ const AddReportModal = () => {
   const [dataReport, setDataReport] = useState("");
   const [dataTitle, setDataTitle] = useState("");
   const [dataDescription, setDataDescription] = useState("");
+  const [dataCountry, setDataCountry] = useState();
 
   const addReportModal = useAddReportModal();
 
@@ -82,7 +84,9 @@ const AddReportModal = () => {
       ) : (
         <>
           <h2 className="mb-6 text-2xl">Location</h2>
-          <div className="pt-3 pb-6 space-y-4">Select</div>
+          <div className="pt-3 pb-6 space-y-4">
+            <SelectCountry value={dataCountry} onChange={} />
+          </div>
         </>
       )}
     </>
