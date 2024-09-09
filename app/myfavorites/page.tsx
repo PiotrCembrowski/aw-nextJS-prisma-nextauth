082b073385880d2bbeeb9028c4598f843f1e173d
Piotr Cembrowski <losttape@gmail.com>
2024-09-09 15:11:29 +0200

    implementing validation

--- app/myfavorites/page.tsx
+++ app/myfavorites/page.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 
 const MyFavoritePage = async () => {
+  if (!userId) {
+    return (
+      <main className="max-w-[1500pz] max-auto px-6 py-12">
+        <p>You need to be authenticated.</p>
+      </main>
+    );
+  }
   return <div>MyFavoritePage</div>;
 };
 
