767b3a2e3e699b3138fd910eca20cc6dcc467a9a
Piotr Cembrowski <losttape@gmail.com>
2024-09-09 15:50:42 +0200

    adding types

--- app/inbox/page.tsx
+++ app/inbox/page.tsx
@@ -1,5 +1,11 @@
 import Conversation from "../components/inbox/Conversation";
 
+export type UserType = {
+  id: string;
+  name: string;
+  avatar_url: string;
+};
+
 const InboxPage = () => {
   return (
     <main className="max-w-[1500px] mx-auto px-6 pb-6 space-y-4">
