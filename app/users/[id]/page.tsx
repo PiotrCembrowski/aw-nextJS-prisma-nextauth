664616e92f66bbb287bae5e81da6a31d74d817b5
Piotr Cembrowski <losttape@gmail.com>
2024-09-09 13:57:20 +0200

    changing routes

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Button from "@/app/components/Button";
+import Button from "@/app/components/ui/Button";
 
 const UserDetailPage: React.FC = () => {
   return (
