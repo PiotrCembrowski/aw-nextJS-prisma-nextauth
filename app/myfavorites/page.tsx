54e55b7d94be5cbd6d9e76fe6bd80ad37a572bba
Piotr Cembrowski <losttape@gmail.com>
2024-09-09 15:17:11 +0200

    adding content

--- app/myfavorites/page.tsx
+++ app/myfavorites/page.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 const MyFavoritePage = async () => {
+  const userId = true;
+
   if (!userId) {
     return (
       <main className="max-w-[1500pz] max-auto px-6 py-12">
@@ -8,7 +10,14 @@ const MyFavoritePage = async () => {
       </main>
     );
   }
-  return <div>MyFavoritePage</div>;
+
+  return (
+    <main className="max-w-[1500pz] max-auto px-6 pb-12">
+      <h1 className="my-6 text-2xl">My favorites</h1>
+
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6"></div>
+    </main>
+  );
 };
 
 export default MyFavoritePage;
