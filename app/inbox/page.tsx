d4834bcc7cb36b90ebb5f525b8bd9445db32475f
Piotr Cembrowski <losttape@gmail.com>
2024-09-09 15:57:02 +0200

    conversaiotn type

--- app/inbox/page.tsx
+++ app/inbox/page.tsx
@@ -6,6 +6,11 @@ export type UserType = {
   avatar_url: string;
 };
 
+export type ConversationType = {
+  id: string;
+  users: UserType;
+};
+
 const InboxPage = () => {
   return (
     <main className="max-w-[1500px] mx-auto px-6 pb-6 space-y-4">
