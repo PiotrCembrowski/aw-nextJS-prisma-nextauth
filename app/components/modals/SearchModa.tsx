176642de4ce043dfd0e2c20f2551b42284ebbf79
Piotr Cembrowski <losttape@gmail.com>
2024-09-10 16:17:26 +0200

    adding modal to searchmodal

--- app/components/modals/SearchModa.tsx
+++ app/components/modals/SearchModa.tsx
@@ -1,5 +1,18 @@
+import Modal from "./Modal";
+import useSearchModal from "@/app/hooks/useSearchModal";
+
 const SearchModa = () => {
-  return <div>SearchModa</div>;
+  let content = <></>;
+  const searchModal = useSearchModal();
+
+  return (
+    <Modal
+      label="Search"
+      content={content}
+      close={searchModal.close}
+      isOpen={searchModal.isOpen}
+    />
+  );
 };
 
 export default SearchModa;
