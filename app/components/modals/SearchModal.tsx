bb1dc831776b3035b59b5e1e51a68217c04bf773
Piotr Cembrowski <losttape@gmail.com>
2024-09-12 15:20:49 +0200

    adding usestate country and imports

--- app/components/modals/SearchModal.tsx
+++ app/components/modals/SearchModal.tsx
@@ -2,10 +2,13 @@
 
 import Modal from "./Modal";
 import useSearchModal from "@/app/hooks/useSearchModal";
+import SelectCountry, { SelectCountryValue } from "../forms/SelectCountry";
+import { useState } from "react";
 
 const SearchModal = () => {
   let content = <></>;
   const searchModal = useSearchModal();
+  const [country, setCountry] = useState<SelectCountryValue>();
 
   return (
     <Modal
