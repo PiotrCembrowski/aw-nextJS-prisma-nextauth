dffa59317acabb29be444267e12aea525e2d169c
Piotr Cembrowski <losttape@gmail.com>
2024-09-18 16:25:12 +0200

    adding onclick func

--- app/components/ui/Categories.tsx
+++ app/components/ui/Categories.tsx
@@ -1,25 +1,63 @@
-import Link from "next/link";
+"use client";
+
+import { useState } from "react";
 
 const Categories = () => {
+  const [continent, setContinent] = useState();
+
+  const settingCountryName = (e: any) => {
+    setContinent(e.target.innerText);
+  };
+
   return (
     <div className="pt-3 cursor-pointer pb-6 flex items-center space-x-12">
-      <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100">
-        <Link href="/">Asia</Link>
+      <div
+        onClick={(e) => {
+          settingCountryName(e);
+        }}
+        className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100"
+      >
+        Asia
       </div>
-      <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100">
-        <Link href="/">Europe</Link>
+      <div
+        onClick={(e) => {
+          settingCountryName(e);
+        }}
+        className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100"
+      >
+        Europe
       </div>
-      <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100">
-        <Link href="/">North America</Link>
+      <div
+        onClick={(e) => {
+          settingCountryName(e);
+        }}
+        className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100"
+      >
+        North America
       </div>
-      <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100">
-        <Link href="/">South America</Link>
+      <div
+        onClick={(e) => {
+          settingCountryName(e);
+        }}
+        className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100"
+      >
+        South America
       </div>
-      <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100">
-        <Link href="/">Africa</Link>
+      <div
+        onClick={(e) => {
+          settingCountryName(e);
+        }}
+        className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100"
+      >
+        Africa
       </div>
-      <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100">
-        <Link href="/">Austalia & Oceania</Link>
+      <div
+        onClick={(e) => {
+          settingCountryName(e);
+        }}
+        className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100"
+      >
+        Austalia & Oceania
       </div>
     </div>
   );
