4da8ee84a40fe3c3b128cf1a2bb01d762faee128
Piotr Cembrowski <losttape@gmail.com>
2024-09-19 17:38:05 +0200

    changes in get func

--- app/api/continent/[continentName]/route.ts
+++ app/api/continent/[continentName]/route.ts
@@ -7,6 +7,13 @@ export const dynamicParams = true;
 
 export async function GET(req: Request, res: Response) {
   const name = req.url;
+  if (name.search("Asia")) {
+    const continent = await prisma.continent.findUnique({
+      where: {
+        name: "Asia",
+      },
+    });
+  }
 
   console.log(name);
   return NextResponse.json(
