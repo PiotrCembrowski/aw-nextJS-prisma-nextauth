967294878ad475d9823fa62c1ae1f6e6277b2e23
Piotr Cembrowski <losttape@gmail.com>
2024-09-26 16:32:04 +0200

    adding working dynamic link

--- app/components/countries/CountriesLisItem.tsx
+++ app/components/countries/CountriesLisItem.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import AsiaImg from "@/public/images/asia.jpg";
 import { CountryType } from "./CountriesList";
+import Link from "next/link";
 
 interface CountryProps {
   country: CountryType;
@@ -8,27 +9,29 @@ interface CountryProps {
 
 const CountriesLisItem: React.FC<CountryProps> = ({ country }) => {
   return (
-    <div className="cursor-pointer">
-      <div className="relative overflow-hidden aspect-square rounded-xl">
-        <Image
-          fill
-          src={AsiaImg}
-          sizes="(max-width: 768px) 768px, (max-width: 1200px):768px, 768px"
-          className="hover:scale-110 object-cover transition h-full w-full"
-          alt="Asia"
-        />
-      </div>
+    <Link href={`/country/${country.name}`}>
+      <div className="cursor-pointer">
+        <div className="relative overflow-hidden aspect-square rounded-xl">
+          <Image
+            fill
+            src={AsiaImg}
+            sizes="(max-width: 768px) 768px, (max-width: 1200px):768px, 768px"
+            className="hover:scale-110 object-cover transition h-full w-full"
+            alt="Asia"
+          />
+        </div>
 
-      <div className="mt-2">
-        <p className="text-lg font-bold">{country.name}</p>
-      </div>
+        <div className="mt-2">
+          <p className="text-lg font-bold">{country.name}</p>
+        </div>
 
-      <div className="mt-2 ">
-        <p className="text-sm text-gray-500">
-          Avg cost of living: <strong>{country.costs}$/month</strong>
-        </p>
+        <div className="mt-2 ">
+          <p className="text-sm text-gray-500">
+            Avg cost of living: <strong>{country.costs}$/month</strong>
+          </p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
