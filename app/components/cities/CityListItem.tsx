0a9f0d9898be6e8aeff2f105e90109d96802dfaa
Piotr Cembrowski <losttape@gmail.com>
2024-09-27 18:01:46 +0200

    skelet of components dynamic/async

--- app/components/cities/CityListItem.tsx
+++ app/components/cities/CityListItem.tsx
@@ -1,11 +1,38 @@
 import { CityType } from "./Citylist";
+import Link from "next/link";
+import Image from "next/image";
+import AsiaImg from "@/public/images/asia.jpg";
 
 interface CityProps {
   city: CityType;
 }
 
-const CityListItem: React.FC<CityProps> = ({ city }) => {
-  return <div>CityListItem</div>;
+const CityListItem: React.FC<CityProps> = async ({ city }) => {
+  return (
+    <Link href={`/city/${city.name}`}>
+      <div className="cursor-pointer">
+        <div className="relative overflow-hidden aspect-square rounded-xl">
+          <Image
+            fill
+            src={AsiaImg}
+            sizes="(max-width: 768px) 768px, (max-width: 1200px):768px, 768px"
+            className="hover:scale-110 object-cover transition h-full w-full"
+            alt="Asia"
+          />
+        </div>
+
+        <div className="mt-2">
+          <p className="text-lg font-bold">{city.name}</p>
+        </div>
+
+        <div className="mt-2 ">
+          <p className="text-sm text-gray-500">
+            Avg cost of living: <strong>{city.costs}$/month</strong>
+          </p>
+        </div>
+      </div>
+    </Link>
+  );
 };
 
 export default CityListItem;
