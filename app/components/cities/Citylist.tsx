df0f13fc0dad6ec6db3673bb4dd906a30c32b815
Piotr Cembrowski <losttape@gmail.com>
2024-09-27 17:27:42 +0200

    adding cityType

--- app/components/cities/Citylist.tsx
+++ app/components/cities/Citylist.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 
+export type CityType = {
+  id: string;
+  name: string;
+  country: string;
+  costs: number;
+  image: string;
+};
+
 const Citylist = () => {
   return <div>Citylist</div>;
 };
