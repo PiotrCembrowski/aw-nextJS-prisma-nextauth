3e9d3eab9b46dd635b0ea28594d91e39b31f0f00
Piotr Cembrowski <losttape@gmail.com>
2024-10-21 14:05:23 +0200

    commenting out navbar featchers

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -11,14 +11,14 @@ const Navbar = async ({ session }) => {
     <nav className="w-full fixed top-0 left-0 py-6 border-b bg-white z-10">
       <div className="max-w-[1500px] mx-auto px-6">
         <div className="flex justify-between items-center">
-          <Link href="/">Around the World</Link>
+          <Link href="/" className="font-bold">
+            Around the World
+          </Link>
 
-          <div className="flex space-x-6 ">
-            <SearchFilters />
-          </div>
+          <div className="flex space-x-6 ">{/* <SearchFilters /> */}</div>
           <div className="flex items-center space-x-6">
             <AddReportButton userId={user} />
-            <UserNav userId={user} />
+            {/* <UserNav userId={user} /> */}
           </div>
         </div>
       </div>
